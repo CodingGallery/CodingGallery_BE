ccc37b30cc3748da333b883f921076bae7c2d4b1
Hans <bangguducks@gmail.com>
2022-06-22 20:58:25 +0900

    sdfsdf

--- src/main/java/com/coding/gallery/repository/ImageReository.java
+++
@@ -1,9 +0,0 @@
-package com.coding.gallery.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.coding.gallery.domain.Image;
-
-public interface ImageReository extends JpaRepository<Image, Image>{
-
-}
